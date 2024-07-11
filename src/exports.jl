51fe02d15b653ed0a127cfb25fcf3b5e37270514
Benjamin Lorenz <lorenz@math.tu-berlin.de>
2024-07-11 10:09:00 +0200

    WIP: posets

--- src/exports.jl
+++ src/exports.jl
@@ -327,6 +327,7 @@ export cellular_primary_decomposition
 export center, has_center, set_center
 export centralizer
 export chain_complex
+export chain_polytope
 export chain_range
 export chamber
 export character_field
@@ -382,6 +383,7 @@ export comm
 export comm!
 export common_components
 export common_refinement
+export comparability_graph
 export complement
 export complement_classes
 export complement_equation
@@ -546,6 +548,7 @@ export exterior_derivative
 export exterior_power
 export f_vector
 export face_fan
+export face_lattice
 export faces
 export facet_degrees
 export facet_indices
@@ -1035,11 +1038,13 @@ export maxes
 export maximal_abelian_quotient, has_maximal_abelian_quotient, set_maximal_abelian_quotient
 export maximal_blocks
 export maximal_cells
+export maximal_chains
 export maximal_cones
 export maximal_extension
 export maximal_groebner_cone
 export maximal_normal_subgroups, has_maximal_normal_subgroups, set_maximal_normal_subgroups
 export maximal_polyhedra, maximal_polyhedra_and_multiplicities
+export maximal_ranked_poset
 export maximal_subgroup_classes, has_maximal_subgroup_classes, set_maximal_subgroup_classes
 export maximal_subgroups
 export metadata
@@ -1179,6 +1184,7 @@ export orbits
 export order, has_order, set_order
 export order_field_of_definition
 export order_of_vanishing
+export order_polytope
 export ordering
 export orders_centralizers
 export orders_class_representatives
@@ -1194,6 +1200,7 @@ export parallel_extension
 export parametrization
 export parametrization_conic
 export parent
+export partially_ordered_set
 export partition
 export partitions
 export patches
